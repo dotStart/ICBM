baae2933794bc0e41bc401b375f34475643dfa8c
Johannes Donath <johannesd@torchmind.com>
2016-12-02 05:49:57 +0100

    Added an annotation to automate API dependencies within dependant mods.

---
+++ src/api/java/tv/dotstart/mc/icbm/api/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Provides external access to some parts of the ICBM implementation.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+@API(owner = "doticbm", apiVersion = "0.1.1", provides = "doticbm|api")
+package tv.dotstart.mc.icbm.api;
+
+import net.minecraftforge.fml.common.API;
